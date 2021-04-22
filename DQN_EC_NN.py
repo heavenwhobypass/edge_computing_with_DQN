38b403c786b716093efe301892b265d20503851a
ttw <907993189@qq.com>
2021-04-22 13:11:39 +0800

    2021-04-22 add a new wrok about complish the paper's work that other had proposed. the first 基于DRL的MEC任务卸载与资源调度算法_薛宁

---
+++ DQN_EC_NN.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+############################
+# File Name: DQN_EC_NN.py
+# Author: taowang
+# mail: 907993189@qq.com
+# Create Time: 2021/4/22 13:09:19
+############################
+
+# its the NetWork about Q_table
---
+++ DQN_EC_main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+############################
+# File Name: DQN_EC_main.py
+# Author: taowang
+# mail: 907993189@qq.com
+# Create Time: 2021/4/22 13:08:28
+############################
+
+# its main file
+# the process of MEC offloading
+
